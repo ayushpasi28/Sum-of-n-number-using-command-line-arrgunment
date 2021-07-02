71dbd4c7b0821793361d5fe27348902d500f5c16
ayushpasi28 <61745106+ayushpasi28@users.noreply.github.com>
2021-07-02 22:32:20 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+
+public class sum
+{
+public static void main(String[] args)
+{
+int s=0;
+for(int i=0;i<args.length;i++)
+{
+s=s+Integer.parseInt(args[i]);
+}
+System.out.println("sum :"+s);
+
+}
+}
